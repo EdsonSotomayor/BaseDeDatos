dac93925736d1c72d454e35fb0ee96629f18c82b
Alumno <Alumno@PC04>
2019-10-08 10:10:03 -0500

    v4

--- EjercicioBaseDeDatos/EjercicioBaseDeDatos/MainWindow.xaml.cs
+++ EjercicioBaseDeDatos/EjercicioBaseDeDatos/MainWindow.xaml.cs
@@ -22,39 +22,38 @@ namespace EjercicioBaseDeDatos
     /// </summary>
     public partial class MainWindow : Window
     {
-        OleDbConnection con;
-        DataTable dt;
+        OleDbConnection con; //AGgregamos la conexion
+        DataTable dt; //
         public MainWindow()
         {
             InitializeComponent();
+            //Conectamos la base de datos a nuestro archivo
             con = new OleDbConnection();
-            con.ConnectionString = "Provider=Microsoft.Jet.Oledb.4.0; Data Source=" + AppDomain.CurrentDomain.BaseDirectory + "\\AlumnosBD.mdb";
+            con.ConnectionString = "Provider=Microsoft.Jet.Oledb.4.0; Data Source=" + AppDomain.CurrentDomain.BaseDirectory + "\\AlumnosDB.mdb";
             MostrarDatos();
         }
         private void MostrarDatos()
         {
             OleDbCommand cmd = new OleDbCommand();
             if (con.State != ConnectionState.Open)
-            {
-                con.Open();
-            }
+                 con.Open();
             cmd.Connection = con;
-             cmd.CommandText = "Select * form Progra";
-                OleDbDataAdapter da = new OleDbDataAdapter();
-                dt = new DataTable();
-                da.Fill(dt);
-                gvDatos.ItemsSource = dt.AsDataView();
-                if (dt.Rows.Count > 0)
-                {
-                    lbContenido.Visibility = System.Windows.Visibility.Hidden;
-                    gvDatos.Visibility = System.Windows.Visibility.Visible;
-                }
-                else
-                {
-                    lbContenido.Visibility = System.Windows.Visibility.Visible;
-                    gvDatos.Visibility = System.Windows.Visibility.Hidden;
-                }
-       
+            cmd.CommandText = "select * from Progra";
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            dt = new DataTable();
+            da.Fill(dt);
+            gvDatos.ItemsSource = dt.AsDataView();
+            if (dt.Rows.Count > 0)
+            {
+                lbContenido.Visibility = System.Windows.Visibility.Hidden;
+                gvDatos.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                lbContenido.Visibility = System.Windows.Visibility.Visible;
+                gvDatos.Visibility = System.Windows.Visibility.Hidden;
+            }
+
         }
         private void LimpiarTodo()
         {
@@ -69,39 +68,90 @@ namespace EjercicioBaseDeDatos
         {
             OleDbCommand cmd = new OleDbCommand();
             if (con.State != ConnectionState.Open)
-            {
                 con.Open();
-            }
+            
             cmd.Connection = con;
-            if (txtId.Text!="")
+
+            if (txtId.Text != "")
             {
-                if (cbGenero.Text!= "Selecciona Genero")
+                if (txtId.IsEnabled == true)
                 {
-                    cmd.CommandText = "insert into Progra(Id,Nombre,Genero,Telefono,Direccion)" +
-                        "Values(" + txtId.Text + ",'" + txtNombre.Text + ",'" + cbGenero.Text + ",'" + txtTelefono.Text + ",'" + txtDireccion + ")'";
-                    cmd.ExecuteNonQuery();
-                    MessageBox.Show("Alumnos agregado correcto...");
-                    LimpiarTodo();
+                    if (cbGenero.Text != "Selecciona Genero")
+                    {
+                        cmd.CommandText = "insert into Progra(Id,Nombre,Genero,Telefono,Direccion) " +
+                           "Values(" + txtId.Text + ",'" + txtNombre.Text + "','" + cbGenero.Text + "'," + txtTelefono.Text + ",'" + txtDireccion.Text + "')";
+                        cmd.ExecuteNonQuery();
+                        MostrarDatos();
+                        MessageBox.Show("Alumno agregado correctamente...");
+                        LimpiarTodo();
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Favor de seleccionar el genero");
+                    }
                 }
                 else
                 {
-                    MessageBox.Show("Favor de seleccionar el genero");
+                    cmd.CommandText = "update Progra set Nombre='" + txtNombre.Text + "',Genero='" + cbGenero.Text + "',Telefono=" + txtTelefono.Text + ",Direccion='" + txtDireccion.Text + "'where Id=" + txtId.Text;
+                    cmd.ExecuteNonQuery();
+                    MostrarDatos();
+                    MessageBox.Show("Datos del alumno Actualizado...");
+                    LimpiarTodo();
                 }
+
             }
-            else
-            {
-                cmd.CommandText = "update Progra set Nombre='" + txtNombre.Text + "',Genero='" + cbGenero.Text + "',Telefono=" + txtTelefono.Text + ",Direccion='" + txtDireccion.Text + "'where Id=" + txtId.Text;
-                MostrarDatos();
-                MessageBox.Show("Datos del alumno Actualizado...");
-                LimpiarTodo();
-            }
-            
-           
         }
 
         private void BtnCancelar_Click(object sender, RoutedEventArgs e)
         {
+            LimpiarTodo();
+        }
 
+        private void BtnEditar_Click(object sender, RoutedEventArgs e)
+        {
+            if (gvDatos.SelectedItems.Count > 0)
+            {
+                DataRowView row = (DataRowView)gvDatos.SelectedItems[0];
+                txtId.Text = row["Id"].ToString();
+                txtNombre.Text = row["Nombre"].ToString();
+                cbGenero.Text = row["Genero"].ToString();
+                txtTelefono.Text = row["Telefono"].ToString();
+                txtDireccion.Text = row["Direccion"].ToString();
+                txtId.IsEnabled = false;
+                btnNuevo.Content = "Actualizar";
+            }
+            else
+            {
+                MessageBox.Show("Favor de seleccionar un alumno...");
+            }
+        }
+
+        private void BtnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            if (gvDatos.SelectedItems.Count > 0)
+            {
+                DataRowView row = (DataRowView)gvDatos.SelectedItems[0];
+                OleDbCommand cmd = new OleDbCommand();
+                if (con.State != ConnectionState.Open)
+                    con.Open();
+                cmd.Connection = con;
+                cmd.CommandText = "delete form Progra where Id=" + row["Id"].ToString();
+                cmd.ExecuteNonQuery();
+                MostrarDatos();
+                MessageBox.Show("Alumnos eliminados correctamente...");
+                LimpiarTodo();
+            }
+            else
+            {
+                MessageBox.Show("Favor de seleccionar un alumno...");
+            }
+        }
+    
+
+        private void BtnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
